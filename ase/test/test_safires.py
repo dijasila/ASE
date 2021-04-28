064dabdf85776d161097baee49c3988d1ae8cee5
bjk24 <bjoern.kirchhoff@protonmail.com>
2021-04-28 14:28:53 +0000

    updating SAFIRES unit test to reflect most recent changes to molecule velocity redistribution

--- ase/test/test_safires.py
+++ ase/test/test_safires.py
@@ -1,63 +1,144 @@
 def test_safires():
-    """Tests the SAFIRES boundary method.
+    """Test for SAFIRES class.
+    
+    SAFIRES is a boundary-based separation scheme
+    for coupled calculations. The fundamental features
+    of the method can however be tested without doing
+    an actual coupled calculation.
 
-    Creates a system of three Lennard-Jones particles
-    with Ar parameters, one being the solute (tag = 0),
-    one representing the inner region (tag = 1), and
-    one the outer region (tag = 2). A boundary event
-    occurs on step 36, so the potential energy and
-    distance between solute and inner region particle
-    at step 37 can be used to assess energy conservation
-    (NVE dynamics) and if the elastic collision is
-    performed as expected.
+    The following unit test includes two tests; one for
+    a simple, monoatomic argon LJ model and another one
+    using H2O and the TIP4P force field.
+
+    The LJ unit test is made redundant by the more
+    complex H2O test. However, it is included to quickly
+    rule out if any observed problem or instability is
+    actually cause by SAFIRES or by changes to the 
+    FixBondLengths or TIP4P classes.
+
+    @bjk24, 2021-02-23.
+    
     """
 
     import numpy as np
     from ase import units
     from ase import Atoms
+    from ase.calculators.tip4p import TIP4P, rOH, angleHOH
+    from ase.constraints import FixBondLengths
+    from ase.constraints import FixAtoms
     from ase.calculators.lj import LennardJones
     from ase.md.verlet import VelocityVerlet
     from ase.md.safires import SAFIRES
-    from ase.constraints import FixAtoms
 
-    # Build MWE model system.
+    # TEST 1: monoatomic LJ liquid
+
+    # Build MWE LJ model system.
     a_cell = 10
     cell = ((a_cell, 0, 0), (0, a_cell, 0), (0, 0, a_cell))
     pbc = (1, 1, 1)
 
     atoms = Atoms("Ar3",
-                  [[(a_cell / 2), (a_cell / 2), (a_cell / 2)],
-                  [(a_cell/2 + 2), (a_cell/2 - 1), (a_cell / 2)],
-                  [(a_cell/2 - 4), (a_cell / 2 + 0.5), (a_cell / 2)]])
-    atoms.set_tags([0,1,2]) # 0: solute, 1: inner region, 2: outer region
+            [[(a_cell / 2), (a_cell / 2), (a_cell / 2)],
+            [(a_cell/2 + 2), (a_cell/2 - 1), (a_cell / 2)],
+            [(a_cell/2 - 4), (a_cell / 2 + 0.5), (a_cell / 2)]])
     atoms.set_cell(cell)
     atoms.set_pbc(pbc)
-    atoms.calc = LennardJones(epsilon=120 * units.kB, sigma=3.4)
+    
+    # Tags: 0 = solute, 1 = inner region, 2 = outer region.
+    atoms.set_tags([0,1,2])
 
-    # Fix constrain solute.
-    fixatoms = FixAtoms(indices=[atom.index for atom in atoms
-                                 if atom.tag == 0])
-    atoms.constraints = ([fixatoms])
+    # Fix solute particle (tag = 0)
+    atoms.constraints = [FixAtoms(indices=[0])]
 
-    # Give atoms initial momentum.
+    # Apply initial momenta.
     atoms[1].momentum = np.asarray([1, 1, 0])
     atoms[2].momentum = np.asarray([2, 0, 0])
-
-    # Initialize Velocity Verlet NVE dynamics.
+    
+    # Initialize calculator and dynamics objects.
+    atoms.calc = LennardJones(epsilon=120 * units.kB,
+                              sigma=3.4, rc=4.9)
     dt = 1.0 * units.fs
     md = VelocityVerlet(atoms, timestep=dt)
 
     # Initialize SAFIRES class.
-    safires = SAFIRES(atoms, mdobject=md, natoms=1, logfile=None)
+    safires = SAFIRES(atoms, mdobject=md, natoms=1,
+                      logfile="md.log")
     md.attach(safires.safires, interval=1)
-
-    # Run.
+    
+    # Run MD.
     md.run(37)
 
-    # Evaluate potential energy and solute -> tag=1 distance.
-    epot_check = -0.0172312389809
+    # Assert results.
+    epot_check = -0.00281226614101
     epot = atoms.calc.results["energy"]
-    d_check = 3.85006966993
+    d_check = 3.84999554045
     d = np.linalg.norm(atoms[0].position - atoms[1].position)
+    assert abs(epot_check - epot) < 1.e-10
+    assert abs(d_check - d) < 1.e-10
+
+    # TEST 2: H2O / TIP4P
+
+    # Reset atoms object.
+    atoms = Atoms()
+
+    # RATTLE constraint to fix bond lengths of H2O molecules.
+    def rigid(atoms):
+        rattle = ([(3 * i + j, 3 * i + (j + 1) % 3)
+                            for i in range(len(atoms) // 3)
+                            for j in [0, 1, 2]])
+
+        rattle = [(c[0], c[1]) for c in rattle]
+        rattle = FixBondLengths(rattle)
+        return rattle
+
+    # Built H2O MWE model systen.
+    a_cell = 10
+    cell = ((a_cell, 0, 0), (0, a_cell, 0), (0, 0, a_cell))
+    pbc = (1, 1, 1)
+
+    x = angleHOH * np.pi / 180 / 2
+    pos = [[5, 5, 5],
+           [5, 5 + rOH*np.cos(x), 5 + rOH*np.sin(x)],
+           [5, 5 + rOH*np.cos(x), 5 - rOH*np.sin(x)],
+           [3.5, 3.5, 3.5],
+           [3.5, 3.5 + rOH*np.cos(x), 3.5 + rOH*np.sin(x)],
+           [3.5, 3.5 + rOH*np.cos(x), 3.5 - rOH*np.sin(x)],
+           [6.7, 6.7, 6.7],
+           [6.7, 6.7 + rOH*np.cos(x), 6.7 + rOH*np.sin(x)],
+           [6.7, 6.7 + rOH*np.cos(x), 6.7 - rOH*np.sin(x)]]
+    atoms = Atoms('OH2OH2OH2', positions=pos, cell=cell, pbc=pbc)
+
+    # Tags: 0 = solute, 1 = inner region, 2 = outer region.
+    atoms.set_tags([0,0,0,1,1,1,2,2,2])
+    
+    # Apply RATTLE constraint.
+    rattle = rigid(atoms)
+    fixatoms = FixAtoms(indices=[0,1,2])
+
+    # Fix central molecule.
+    atoms.constraints = ([fixatoms] + [rattle])
+
+    # Apply initial momenta.
+    atoms[3].momentum = np.asarray([-1, 1, 0])
+    atoms[6].momentum = np.asarray([-1, -1, -1])
+
+    # Initialize calculator and dynamics objects.
+    atoms.calc = TIP4P(rc=4.9)
+    dt = 1.0 * units.fs
+    md = VelocityVerlet(atoms, timestep=dt)
+
+    # Initialize SAFIRES class.
+    safires = SAFIRES(atoms, mdobject=md, natoms=3, 
+                      logfile="md.log")
+    md.attach(safires.safires, interval=1)
+    
+    # Run MD.
+    md.run(28)
+    
+    # Assert results.
+    epot_check = -0.0763445456798
+    epot = atoms.calc.results["energy"]
+    d_check = 2.71021718223
+    d = np.linalg.norm(atoms[0].position - atoms[3].position)
     assert abs(epot_check - epot) < 1.e-10
     assert abs(d_check - d) < 1.e-10
