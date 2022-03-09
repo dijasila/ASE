188376297b453e03d0e390da39c3da9cbcdb2376
Igor Kowalec <aikey2011@gmail.com>
2022-03-09 11:32:02 +0000

    Revert "Update ase/gui/modify.py"
    This reverts commit 6224095f00f85080615b90c6f2433a4ccf5c5a39

--- ase/gui/modify.py
+++ ase/gui/modify.py
@@ -49,8 +49,8 @@ class ModifyAtoms:
         import numpy as np
         selected = self.selection()
         indices = np.where(selected == True)[0]
-        selection_len = len([i for i in selected if i])
-        if selection_len != 2:
+        selection_len = len([i for i in selected if i == True])
+        if not selection_len == 2:
             ui.error(_('Only two atoms must be selected!'))
             return
 
