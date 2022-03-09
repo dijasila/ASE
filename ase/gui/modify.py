6d7d6369e3eb51a502499ecde2f6ba4d73ef6d83
Igor Kowalec <aikey2011@gmail.com>
2022-03-09 11:32:20 +0000

    Revert "Updated description"
    This reverts commit cbc642e62bf7f625f2f9b47d2afb26261006862e

--- ase/gui/modify.py
+++ ase/gui/modify.py
@@ -9,8 +9,7 @@ from ase.gui.utils import get_magmoms
 
 class ModifyAtoms:
     """Presents a dialog box where the user is able to change the
-    atomic type, the magnetic moment and tags of the selected atoms,
-    and switch atomic indices in preparation of input for NEB calculations.
+    atomic type, the magnetic moment and tags of the selected atoms.
     """
     def __init__(self, gui):
         self.gui = gui
