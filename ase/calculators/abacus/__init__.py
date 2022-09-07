61beb5578ba01846a6caf5fff0175118c72e4cfb
jiyuyang <jiyuyang@mail.ustc.edu.cn>
2022-09-07 21:58:38 +0800

    modify __init__.py

--- ase/calculators/abacus/__init__.py
+++ ase/calculators/abacus/__init__.py
@@ -2,10 +2,11 @@
 Created on Mon Apr 23 14:30:24 2018
 
 @author: Shen Zhen-Xiong
+@author: Yuyang Ji
 """
 
 
-from .abacus import Abacus, get_abacus_version
+from .abacus import Abacus, AbacusProfile, AbacusTemplate, get_abacus_version
 from .create_input import AbacusInput
-__all__ = ['Abacus', 'AbacusInput', 'get_abacus_version']
-
+__all__ = ['Abacus', 'AbacusInput', 'AbacusProfile',
+           'AbacusTemplate', 'get_abacus_version']
