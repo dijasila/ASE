84b82c945a5ae8c8ccaa308daa8d0f61782a703d
jiyuyang <jiyuyang@mail.ustc.edu.cn>
2022-09-08 23:35:39 +0800

    update pytest

--- ase/test/calculator/abacus/test_abacus_Al_volrelax.py
+++ ase/test/calculator/abacus/test_abacus_Al_volrelax.py
@@ -12,22 +12,22 @@ calc = pytest.mark.calculator
 @calc('abacus')
 def test_abacus_Al_volrelax(factory):
     """
-    Run ABACUS tests to ensure that relaxation with the VASP calculator works.
+    Run ABACUS tests to ensure that relaxation with the ABACUS calculator works.
 
     """
 
-    kpts = [4,4,4]
+    kpts = [4, 4, 4]
     command = 'mpirun -n 28 abacus'
-    input_param = {'ntype': 1, 'ecutwfc':20, 'scf_nmax':50, 'smearing_method':'gaussian',
-                    'smearing_sigma':0.01, 'basis_type':'lcao', 'ks_solver':'genelpa',
-                    'mixing_type':'pulay', 'mixing_beta':0.7, 'scf_thr':1e-8, 'out_chg':1,
-                    'xc':'pbe'}
+    input_param = {'ntype': 1, 'ecutwfc': 20, 'scf_nmax': 50, 'smearing_method': 'gaussian',
+                   'smearing_sigma': 0.01, 'basis_type': 'lcao', 'ks_solver': 'genelpa',
+                   'mixing_type': 'pulay', 'mixing_beta': 0.7, 'scf_thr': 1e-8, 'out_chg': 1,
+                   'xc': 'pbe'}
 
     # -- Perform Volume relaxation within Vasp
     def abacus_vol_relax():
         Al = bulk('Al', 'fcc', a=4.5, cubic=True)
-        calc = factory.calc(calculation='cell-relax', force_thr_ev=0.01, 
-                            stress_thr=10, cal_force=1, cal_stress=1, out_stru=1, 
+        calc = factory.calc(calculation='cell-relax', force_thr_ev=0.01,
+                            stress_thr=10, cal_force=1, cal_stress=1, out_stru=1,
                             kpts=kpts, **input_param)
         calc.command = command
         Al.calc = calc
@@ -51,7 +51,7 @@ def test_abacus_Al_volrelax(factory):
     # -- Volume relaxation using ASE with Abacus as force/stress calculator
     def ase_vol_relax():
         Al = bulk('Al', 'fcc', a=4.5, cubic=True)
-        calc = factory.calc(calculation='scf', cal_force=1, cal_stress=1, out_stru=1, 
+        calc = factory.calc(calculation='scf', cal_force=1, cal_stress=1, out_stru=1,
                             kpts=kpts, **input_param)
         calc.command = command
         Al.calc = calc
@@ -68,10 +68,10 @@ def test_abacus_Al_volrelax(factory):
     def cells_almost_equal(cellA, cellB, tol=0.01):
         return (np.abs(cellA - cellB) < tol).all()
 
-    Al_vasp = abacus_vol_relax()
+    Al_abacus = abacus_vol_relax()
     Al_ase = ase_vol_relax()
 
-    assert cells_almost_equal(Al_ase.get_cell(), Al_vasp.get_cell())
+    assert cells_almost_equal(Al_ase.get_cell(), Al_abacus.get_cell())
 
     # Cleanup
     Al_ase.calc.clean()
--- ase/test/factories.py
+++ ase/test/factories.py
@@ -59,7 +59,7 @@ class AbacusFactory:
         self.basis_dir = basis_dir
 
     def calc(self, **kwargs):
-        from ase.calculators.abacus import Abacus
+        from ase.calculators.abacus import Abacus, AbacusProfile
         pp = {}
         for path in self.pp_dir.glob('*.UPF'):
             fname = path.name
@@ -72,7 +72,8 @@ class AbacusFactory:
             # Names are e.g. Al_gga_10au_100Ry_3s3p2d.orb
             symbol = fname.split('_', 1)[0]
             basis[symbol] = fname
-        return Abacus(command=self.executable, pp=pp, basis=basis, pseudo_dir=self.pp_dir, basis_dir=self.basis_dir, **kwargs)
+        profile = AbacusProfile([self.executable])
+        return Abacus(profile=profile, pp=pp, basis=basis, pseudo_dir=self.pp_dir, basis_dir=self.basis_dir, **kwargs)
 
     def version(self):
         from ase.calculators.abacus import get_abacus_version
@@ -84,6 +85,7 @@ class AbacusFactory:
         paths = config.datafiles['abacus']
         assert len(paths) == 2
         return cls(config.executables['abacus'], paths[0], paths[1])
+
 
 @factory('abinit')
 class AbinitFactory:
