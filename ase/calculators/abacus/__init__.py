25689ac56f912ba7b5dc12b28ab92b53ce3ed647
jiyuyang <jiyuyang@mail.ustc.edu.cn>
2022-09-18 16:47:27 +0800

    add spap function

--- ase/calculators/abacus/__init__.py
+++ ase/calculators/abacus/__init__.py
@@ -8,5 +8,7 @@ Created on Mon Apr 23 14:30:24 2018
 
 from .abacus import Abacus, AbacusProfile, AbacusTemplate, get_abacus_version
 from .create_input import AbacusInput
+from .spap_analysis import spap_analysis
 __all__ = ['Abacus', 'AbacusInput', 'AbacusProfile',
-           'AbacusTemplate', 'get_abacus_version']
+           'AbacusTemplate', 'get_abacus_version',
+           'spap_analysis']
---
+++ ase/calculators/abacus/spap_analysis.py
@@ -0,0 +1,61 @@
+from ase.io import read
+
+
+def get_structure_list(filelist=[], format=None):
+    """Get a list of Atoms objects
+
+    Arguments:
+
+    filelist: list
+        A list of files
+
+    format: str 
+        Used to specify the file-format. Same argument of ase.io.read.
+    """
+    return [read(file, index=-1, format=format) for file in filelist]
+
+
+def spap_analysis(filelist=[], format=None, symprec=0.1, **kwargs):
+    """Structure Prototype Analysis Package used here to analyze symmetry and compare similarity of large amount of atomic structures.
+
+    Description:
+
+    The structure prototype analysis package (SPAP) can analyze symmetry and compare similarity of a large number of atomic structures. 
+    Typically, SPAP can analyze structures predicted by CALYPSO (www.calypso.cn). We use spglib to analyze symmetry. The coordination 
+    characterization function (CCF) is used to measure structural similarity. We developed a unique and advanced clustering method to 
+    automatically classify structures into groups. If you use this program and method in your research, please read and cite the publication: 
+
+        Su C, Lv J, Li Q, Wang H, Zhang L, Wang Y, Ma Y. Construction of crystal structure prototype database: methods and applications. 
+        J Phys Condens Matter. 2017 Apr 26;29(16):165901. doi: 10.1088/1361-648X/aa63cd
+
+
+    Installation:
+
+        1. Use command: `pip install spap`
+        2. Download the source code from https://github.com/chuanxun/StructurePrototypeAnalysisPackage, then install with command `python3 setup.py install`
+        3. We also include module `ccf` and `spap` in ase.calculators.abacus, but these can't be updated quickly with github 
+
+    Parameters
+    ----------
+    filelist: list
+        A list of files
+
+    format: str 
+        Used to specify the file-format. Same argument of ase.io.read.
+
+    symprec: float
+        This precision is used to analyze symmetry of atomic structures.
+
+    **kwargs:
+        More parameters can be found in ase.calculators.abacus.spap
+    """
+    try:
+        from spap import run_spap
+    except ImportError:
+        from .spap import run_spap
+
+    kwargs.pop('structure_list')
+    kwargs.pop('imode')
+    structure_list = get_structure_list(filelist, format)
+    imode = 4
+    run_spap(symprec=symprec, structure_list=structure_list, i_mode=imode)
