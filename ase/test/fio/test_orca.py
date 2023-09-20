2f0bb45dd44cd878f1a65b32ddaa47f70da70332
Jakub Fojt <jakub.fojt96@gmail.com>
2023-09-20 08:58:31 +0200

    Test parsing of dipole

--- ase/test/fio/test_orca.py
+++ ase/test/fio/test_orca.py
@@ -3,6 +3,9 @@ import pytest
 from ase.atoms import Atoms
 from ase.calculators.calculator import compare_atoms
 from ase.io.orca import write_orca, read_geom_orcainp, read_orca_outputs
+from pathlib import Path
+
+parent = Path(__file__).parents[1]
 
 
 def test_orca_inputfile():
@@ -123,6 +126,23 @@ FINAL SINGLE POINT ENERGY       -76.422436201230
     results_sample['free_energy'] = results_sample['energy']
 
     results = read_orca_outputs('.', 'orcamolecule_test.out')
+
+    keys = set(results)
+    assert keys == set(results_sample)
+
+    for key in keys:
+        # each result can be either float or ndarray.
+        assert results[key] == pytest.approx(results_sample[key])
+
+
+def test_read_orca_outputs_with_dipole():
+    results_sample = {
+        'energy': -22203.395391088576,
+        'dipole': np.array([1.66861746e+00, -1.42348670e-03, 1.93467188e-02])}
+
+    results_sample['free_energy'] = results_sample['energy']
+
+    results = read_orca_outputs('.', parent / 'testdata/orca/orca_out_tail.out')
 
     keys = set(results)
     assert keys == set(results_sample)
