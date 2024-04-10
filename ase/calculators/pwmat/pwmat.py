b22cb89fce88a6f9e09701034bd2fcae27daa4e2
lhycms <1404293933@qq.com>
2024-04-10 19:54:35 +0800

    Add PWmat module (io, calculator)

---
+++ ase/calculators/pwmat/pwmat.py
@@ -0,0 +1,309 @@
+"""Define a calculator for PWmat"""
+from __future__ import annotations
+import io
+import os 
+import subprocess
+from typing import Union, Optional, List, Dict, Any, Tuple
+from pathlib import Path
+from contextlib import contextmanager
+
+from ase.calculators import calculator
+from ase.calculators.calculator import Calculator
+from ase.calculators.singlepoint import SinglePointDFTCalculator
+from ase.calculators.pwmat import GeneratePWmatInput
+from ase.atoms import Atoms
+from ase.config import cfg
+from ase.io import read
+
+__author__ = "Hanyu Liu"
+__email__ = "domainofbuaa@gmail.com"
+__date__ = "2024-1-17"
+
+
+
+class PWmat(GeneratePWmatInput, Calculator):
+    """ASE interface for the PWmat, with the Calculator interface.
+    
+    Parameters:
+    
+    atoms:  object
+        Attach an atoms object to the calculator.
+
+    job: str
+        The task type of PWmat to execute
+    
+    label: str
+        Prefix for the output file, Default is 'pwmat'.
+
+    directory: str
+        Set the working directory. Is prepended to ``label``.
+
+    restart: str or bool
+        Sets a label for the directory to load files from.
+        if :code:`restart=True`, the working directory from
+        ``directory`` is used.
+
+    txt: bool, None, str or writable object
+        - If txt is None, output stream will be supressed
+
+        - If txt is '-' the output will be sent through stdout
+
+        - If txt is a string a file will be opened,\
+            and the output will be sent to that file.
+
+        - Finally, txt can also be a an output stream,\
+            which has a 'write' attribute.
+
+        Default is 'pwmat.out'
+    
+        - Requsite:
+            $ export PWMAT_PP_PATH=<your_path_to_pp>/PseudoPotential
+            $ export ASE_PWMAT_COMMAND="mpirun -np 1 PWmat| tee output"
+        - Examples:
+            >>> from ase.io import read
+            >>> from ase.calculator.pwmat import pwmat
+            >>> atoms = read("./atom.config")
+            >>> pwmat = PWmat(atoms=atoms, job='scf', parallel=[4, 1], directory=".")
+            >>> pwmat.calculate()  # launch the task.
+    
+    command: str
+        Custom instructions on how to execute PWmat. Has priority over
+        environment variables.
+    """
+    
+    # Environment commands
+    env_commands: Tuple[str] = ('ASE_PWMAT_COMMAND', 'PWMAT_COMMAND', 'PWMAT_SCRIPT')
+    
+    def __init__(self,
+                 atoms: Optional[Union[Atoms, None]] = None,
+                 job: str = "scf",
+                 parallel: List[int] = [4, 1],
+                 restart: Optional[str] = None,
+                 directory: Optional[str] = ".",
+                 label: Optional[str] = "pwmat",
+                 ignore_bad_restart_file=Calculator._deprecated,
+                 command: Optional[str] = None,
+                 txt: Optional[str] = 'pwmat.out',
+                 **kwargs):
+        assert(len(parallel) == 2)
+        self.parallel: List[int] = parallel
+        self.atoms = atoms
+        self.results: Dict[str, Any] = {}
+        self.kwargs = kwargs
+        # Initialize parameter dictionaries
+        GeneratePWmatInput.__init__(self, job=job)
+        self._store_param_state()   # Initialize an empty parameter state
+        
+        # Store calculator from vasprun.xml here - None => uninitialized
+        self._xml_calc: SinglePointDFTCalculator = None
+        
+        # Set directory and label
+        '''
+        Examples to explain `directory`, `label`
+        
+        * label='abc': (directory='.', prefix='abc')
+        * label='dir1/abc': (directory='dir1', prefix='abc')
+        * label=None: (directory='.', prefix=None)
+        '''
+        self.directory: str = directory
+        if "/" in label:
+            if self.directory != ".":
+                msg = (
+                    'Directory redundantly specified though directory='
+                    f'"{self.directory}" and label="{label}".  Please omit '
+                    '"/" in label.'
+                )
+                raise ValueError(msg)
+            self.label: str = label
+        else:
+            self.prefix: str = label # The label should only contain the prefix
+        
+        if isinstance(restart, bool):
+            restart: Union[str, None] = self.label if restart is True else None
+                    
+        Calculator.__init__(
+            self,
+            restart=restart,
+            ignore_bad_restart_file=ignore_bad_restart_file,
+            label=self.label,
+            atoms=atoms,
+            **kwargs)
+        self.command: str = command
+        self._txt = None
+        self.txt = txt  # Set the output txt stream
+        self.version = None
+        self.atoms = atoms
+
+    def write_input(self, atoms: Atoms):
+        """Write all inputfiles"""
+        # Create the folders where we write the files, if we aren't in the 
+        # current working directory
+        if self.directory != os.curdir and not os.path.isdir(self.directory):
+            os.makedirs(self.directory)
+        
+        self.initialize(atoms)
+        self.create_inputfiles(atoms=atoms, directory=self.directory, parallel=self.parallel, kwargs=self.kwargs)
+
+    def initialize(self, atoms: Atoms):
+        self.atoms = atoms
+        self.natoms = len(atoms)
+
+    def calculate(self, atoms: Union[Atoms, None] = None):
+        """Do a PWmat calculation in the specified directory.
+        
+        This will generate the necessary PWmat input files, and then
+        execute PWmat. After execution, the energy, forces. etc. are read
+        from the PWmat output files.
+        """
+        command: str = self.make_command(self.command)
+        
+        if atoms is not None:
+            self.atoms = atoms.copy()
+        self.write_input(self.atoms)
+        
+        with self._txt_outstream() as out:
+            errorcode = self._run(command=command,
+                                  out=out,
+                                  directory=self.directory)
+        
+        if errorcode:
+            raise calculator.CalculationFailed(
+                '{} in {} returned an error: {:d}'.format(
+                    self.name, Path(self.directory).resolve(), errorcode))
+        # Read results from calculation: Temporarily not implemented.
+                
+    def _run(self, 
+             command: Union[str, None]=None, 
+             out: Union[io.TextIOWrapper, None]=None, 
+             directory: Union[str, None]=None) -> int:
+        """Method to explicitly execute PWmat"""
+        if command is None:
+            command = self.command
+        if directory is None:
+            directory = self.directory
+        errorcode: int = subprocess.call(command,
+                                         shell=True,
+                                         stdout=out,
+                                         cwd=directory)
+        return errorcode
+
+    def make_command(self, command:Union[str, None] = None) -> str:
+        """Return command if one is passed, otherwise try to find 
+        ASE_PWMAT_COMMAND, PWMAT_COMMAND or PWMAT_SCRIPT.
+        If none are set, a CalculatorSetUpError is raised"""
+        cmd: str = ""
+        if command:
+            cmd = command
+        else:
+            # Search for the environment commands
+            for env in self.env_commands:
+                if env in cfg:
+                    cmd = cfg[env].replace("PREFIX", self.prefix)
+        return cmd
+    
+    def read(self, label: Union[str, None] = None):
+        """Read results from PWmat output files.
+        
+        Files which are read: 
+            - final.config
+            - REPORT (No implementation now)
+            
+        Raises ReadError if they are not found"""
+        if label is None:
+            label = self.label
+        Calculator.read(self, label)
+        
+        # If we restart, self.parameetrs isn't initialized
+        if self.parameters is None:
+            self.parameters = self.get_default_parameters()
+        
+        # Check for existence of the necessary output files
+        for f in []:
+            file = self._indir(filename=f)
+            if not file.is_file():
+                raise calculator.ReadError(
+                    f'PWmat outputfile {file} was not found')
+        # Read atoms
+        self.atoms = self.read_atoms(filename=self._indir("final.config"))
+        # Read parameters. No implementation now.
+        # Read results from the calculation. No implementation now.
+            
+    def _indir(self, filename: str):
+        "Prepend current directory to filename"
+        return Path(self.directory) / filename
+        
+    def _store_param_state(self) -> None:
+        """Store current parameter state."""
+        self.param_state: Dict[str, Dict[str, Any]] = dict(
+            parallel_params=self.parallel_params.copy(),
+            float_params=self.float_params.copy(),
+            int_params=self.int_params.copy(),
+            bool_params=self.bool_params.copy(),
+            char_params=self.char_params.copy(),
+            special_params=self.special_params.copy(),
+            string_params=self.string_params.copy(),
+            list_int_params=self.list_int_params.copy(),
+            list_float_params=self.list_float_params.copy())
+
+    @contextmanager
+    def _txt_outstream(self):
+        """Custom function for opening a text output stream. Uses self.txt
+        to determine the output stream, and accepts a string or an open
+        writable object.
+        If a string is used, a new stream is opened, and automatically closes
+        the new stream again when exiting.
+
+        Examples:
+        # Pass a string
+        calc.txt = 'vasp.out'
+        with calc.txt_outstream() as out:
+            calc.run(out=out)   # Redirects the stdout to 'vasp.out'
+
+        # Use an existing stream
+        mystream = open('vasp.out', 'w')
+        calc.txt = mystream
+        with calc.txt_outstream() as out:
+            calc.run(out=out)
+        mystream.close()
+
+        # Print to stdout
+        calc.txt = '-'
+        with calc.txt_outstream() as out:
+            calc.run(out=out)   # output is written to stdout
+        """
+
+        txt = self.txt
+        open_and_close = False  # Do we open the file?
+
+        if txt is None:
+            # Suppress stdout
+            out = subprocess.DEVNULL
+        else:
+            if isinstance(txt, str):
+                if txt == '-':
+                    # subprocess.call redirects this to stdout
+                    out = None
+                else:
+                    # Open the file in the work directory
+                    txt = self._indir(txt)
+                    # We wait with opening the file, until we are inside the
+                    # try/finally
+                    open_and_close = True
+            elif hasattr(txt, 'write'):
+                out = txt
+            else:
+                raise RuntimeError('txt should either be a string'
+                                   'or an I/O stream, got {}'.format(txt))
+
+        try:
+            if open_and_close:
+                out = open(txt, 'w')
+            yield out
+        finally:
+            if open_and_close:
+                out.close()
+    
+    def read_atoms(self, filename: str):
+        """Read the atoms from file located in the PWmat
+        working directory. Normally called final.config."""
+        return read(filename)
---
+++ ase/test/fio/test_pwmat.py
@@ -0,0 +1,208 @@
+import io
+import os
+import unittest
+
+from ase import Atoms
+from ase.io import read
+
+# atom.config content
+atom_config_string: str = '''  12 atoms
+ Lattice vector (Angstrom)
+   3.190316E+00      5.525789E+00      0.000000E+00  
+   -6.380631E+00     0.000000E+00      0.000000E+00  
+   0.000000E+00      0.000000E+00      2.312977E+01  
+ Position (normalized), move_x, move_y, move_z
+  42         0.333333           0.166667           0.500000       1  1  1
+  16         0.166667           0.333333           0.432343       1  1  1
+  16         0.166667           0.333333           0.567657       1  1  1
+  42         0.333333           0.666667           0.500000       1  1  1
+  16         0.166667           0.833333           0.432343       1  1  1
+  16         0.166667           0.833333           0.567657       1  1  1
+  42         0.833333           0.166667           0.500000       1  1  1
+  16         0.666667           0.333333           0.432343       1  1  1
+  16         0.666667           0.333333           0.567657       1  1  1
+  42         0.833333           0.666667           0.500000       1  1  1
+  16         0.666667           0.833333           0.432343       1  1  1
+  16         0.666667           0.833333           0.567657       1  1  1
+MAGNETIC
+42 0.0
+16 0.0
+16 0.0
+42 0.0
+16 0.0
+16 0.0
+42 0.0
+16 0.0
+16 0.0
+42 0.0
+16 0.0
+16 0.0
+'''
+
+# REPORT CONTENT
+report_string: str = '''           1           1
+ PRECISION  = AUTO       
+ JOB       = SCF
+ IN.PSP1   = Mo.SG15.PBE.UPF
+ IN.PSP2   = S.SG15.PBE.UPF
+ IN.ATOM   = atom.config    
+Weighted average num_of_PW for all kpoint=                        32866.440
+ ************************************
+ E_Hxc(eV)         36168.0440410622     
+ E_ion(eV)        -79287.9353888918     
+ E_Coul(eV)        37531.6123362230     
+ E_Hxc+E_ion(eV)  -43119.8913478296     
+ NONSCF     1          AVE_STATE_ERROR= 0.1278E+01
+ NONSCF     2          AVE_STATE_ERROR= 0.4100E+00
+ NONSCF     3          AVE_STATE_ERROR= 0.5897E-01
+ NONSCF     4          AVE_STATE_ERROR= 0.1027E-01
+ NONSCF     5          AVE_STATE_ERROR= 0.1746E-02
+ NONSCF     6          AVE_STATE_ERROR= 0.3660E-03
+ iter=   7   ave_lin=  4.0  iCGmth=   3
+ Ef(eV)               = 0.1580784E+01
+ err of ug            = 0.1865E-04
+ dv_ave, drho_tot     = 0.0000E+00 0.1115E+00
+ E_tot(eV)            = -.97107467802733E+04    -.9711E+04
+ -------------------------------------------
+ iter=   8   ave_lin=  4.0  iCGmth=   3
+ Ef(eV)               = 0.1668484E+00
+ err of ug            = 0.1740E-02
+ dv_ave, drho_tot     = 0.2351E-01 0.4224E-01
+ E_tot(eV)            = -.97012791705132E+04    0.9468E+01
+ -------------------------------------------
+ iter=   9   ave_lin=  4.0  iCGmth=   3
+ Ef(eV)               = -.1843762E+00
+ err of ug            = 0.5185E-03
+ dv_ave, drho_tot     = 0.1239E-01 0.2571E-01
+ E_tot(eV)            = -.97011992106859E+04    0.7996E-01
+ -------------------------------------------
+ iter=  10   ave_lin=  4.0  iCGmth=   3
+ Ef(eV)               = 0.6739299E-01
+ err of ug            = 0.1810E-03
+ dv_ave, drho_tot     = 0.1330E-01 0.1094E-01
+ E_tot(eV)            = -.97011506947260E+04    0.4852E-01
+ -------------------------------------------
+ iter=  11   ave_lin=  4.0  iCGmth=   3
+ Ef(eV)               = 0.1051469E+00
+ err of ug            = 0.1704E-03
+ dv_ave, drho_tot     = 0.7131E-02 0.1147E-01
+ E_tot(eV)            = -.97011441364653E+04    0.6558E-02
+ -------------------------------------------
+ iter=  12   ave_lin=  4.0  iCGmth=   3
+ Ef(eV)               = -.3913156E+00
+ err of ug            = 0.7956E-04
+ dv_ave, drho_tot     = 0.7296E-02 0.6186E-02
+ E_tot(eV)            = -.97011237684088E+04    0.2037E-01
+ -------------------------------------------
+ iter=  13   ave_lin=  4.0  iCGmth=   3
+ Ef(eV)               = -.2498529E+00
+ err of ug            = 0.3156E-04
+ dv_ave, drho_tot     = 0.7603E-02 0.5397E-02
+ E_tot(eV)            = -.97011183876936E+04    0.5381E-02
+ -------------------------------------------
+ iter=  14   ave_lin=  4.0  iCGmth=   3
+ Ef(eV)               = -.5993476E+00
+ err of ug            = 0.3795E-04
+ dv_ave, drho_tot     = 0.1014E-01 0.4787E-02
+ E_tot(eV)            = -.97011108205844E+04    0.7567E-02
+ -------------------------------------------
+ iter=  15   ave_lin=  4.0  iCGmth=   3
+ Ef(eV)               = -.6440977E+00
+ err of ug            = 0.4144E-04
+ dv_ave, drho_tot     = 0.6350E-02 0.3427E-02
+ E_tot(eV)            = -.97011071628981E+04    0.3658E-02
+ -------------------------------------------
+ iter=  16   ave_lin=  4.0  iCGmth=   3
+ Ef(eV)               = -.7845993E+00
+ err of ug            = 0.2698E-04
+ dv_ave, drho_tot     = 0.1169E-01 0.2450E-02
+ E_tot(eV)            = -.97010947399116E+04    0.1242E-01
+ -------------------------------------------
+ iter=  17   ave_lin=  4.0  iCGmth=   3
+ Ef(eV)               = -.8289866E+00
+ err of ug            = 0.1184E-04
+ dv_ave, drho_tot     = 0.7705E-02 0.2555E-02
+ E_tot(eV)            = -.97010894449507E+04    0.5295E-02
+ -------------------------------------------
+ iter=  18   ave_lin=  4.0  iCGmth=   3
+ Ef(eV)               = -.8828054E+00
+ err of ug            = 0.1682E-04
+ dv_ave, drho_tot     = 0.1111E-01 0.1701E-02
+ E_tot(eV)            = -.97010782531264E+04    0.1119E-01
+ -------------------------------------------
+ iter=  19   ave_lin=  3.1  iCGmth=   3
+ Ef(eV)               = -.9222894E+00
+ err of ug            = 0.8819E-05
+ dv_ave, drho_tot     = 0.1039E-01 0.1447E-02
+ E_tot(eV)            = -.97010749755841E+04    0.3278E-02
+ -------------------------------------------
+ iter=  20   ave_lin=  4.0  iCGmth=   3
+ Ef(eV)               = -.9883360E+00
+ err of ug            = 0.8409E-05
+ dv_ave, drho_tot     = 0.1329E-01 0.1361E-02
+ E_tot(eV)            = -.97010669845761E+04    0.7991E-02
+ -------------------------------------------
+ iter=  21   ave_lin=  4.0  iCGmth=   3
+ Ef(eV)               = -.1034416E+01
+ err of ug            = 0.7729E-05
+ dv_ave, drho_tot     = 0.1561E-01 0.1309E-02
+ E_tot(eV)            = -.97010675551896E+04    -.5706E-03
+ -------------------------------------------
+ iter=  22   ave_lin=  2.6  iCGmth=   3
+ Ef(eV)               = -.1060063E+01
+ err of ug            = 0.9139E-05
+ dv_ave, drho_tot     = 0.1363E-01 0.1270E-02
+ E_tot(eV)            = -.97010647495656E+04    0.2806E-02
+ -------------------------------------------
+ iter=  23   ave_lin=  4.0  iCGmth=   3
+ Ef(eV)               = -.1104588E+01
+ err of ug            = 0.9866E-05
+ dv_ave, drho_tot     = 0.1660E-01 0.1190E-02
+ E_tot(eV)            = -.97010616934368E+04    0.3056E-02
+ -------------------------------------------
+ iter=  24   ave_lin=  4.0  iCGmth=   3
+ Ef(eV)               = -.1177740E+01
+ err of ug            = 0.1142E-04
+ dv_ave, drho_tot     = 0.1747E-01 0.1088E-02
+ E_tot(eV)            = -.97010602913740E+04    0.1402E-02
+ -------------------------------------------
+ iter=  25   ave_lin=  4.0  iCGmth=   3
+ Ef(eV)               = -.1202988E+01
+ err of ug            = 0.5497E-05
+ dv_ave, drho_tot     = 0.1582E-01 0.8864E-03
+ E_tot(eV)            = -.97010581022804E+04    0.2189E-02
+ -------------------------------------------
+ iter=  26   ave_lin=  3.2  iCGmth=   3
+ Ef(eV)               = -.1256735E+01
+ err of ug            = 0.6583E-05
+ dv_ave, drho_tot     = 0.1712E-01 0.7097E-03
+ E_tot(eV)            = -.97010581505657E+04    -.4829E-04
+ -------------------------------------------
+'''
+
+
+class TestReadPWmat(unittest.TestCase):
+    def setUp(self):
+        self.atom_config_path: str = "atom.config"
+        self.report_path: str = "REPORT"
+        with open(self.atom_config_path, 'w') as f:
+            f.write(atom_config_string)
+        with open(self.report_path, 'w') as f:
+            f.write(report_string)
+            
+    def test_read_pwmat(self):
+        atoms: Atoms = read("atom.config")
+        self.assertEqual(len(atoms), 12)
+        
+    def test_read_report(self):
+        report = read(self.report_path, index=-1)   # Get last step of scf
+        self.assertEqual(report.get_potential_energy(), -.97010581505657E+04)
+        
+    def tearDown(self):
+        if os.path.isfile(self.atom_config_path):
+            os.remove(self.atom_config_path)
+        if os.path.isfile(self.report_path):
+            os.remove(self.report_path)
+
+if __name__ == "__main__":
+    unittest.main()
