9fc6868deaea94e67b4c12aa89e9c23ead2e6c6f
Ryuichi Arafune <ryuichi.arafune@gmail.com>
2024-04-12 20:56:17 +0900

    🐛  scipy befor 1.7.0 does not accept degrees arg.

--- ase/test/dft/test_bz.py
+++ ase/test/dft/test_bz.py
@@ -2,6 +2,7 @@ from matplotlib.testing.compare import compare_images
 from pathlib import Path
 import matplotlib.pyplot as plt
 from scipy.spatial.transform import Rotation
+import numpy as np
 from ase.lattice import HEX2D
 
 
@@ -12,9 +13,9 @@ def test_repeat_transpose_bz():
     """Testint plot_bz."""
 
     hex2d = HEX2D(a=1.0)
-    r = Rotation.from_rotvec([0, 0, 10], True)
+    r = Rotation.from_rotvec([0, 0, np.deg2rad(10)])
     fig, ax = plt.subplots()
-    hex2d.plot_bz(repeat=(2, 1), transforms=[r])
+    hex2d.plot_bz(repeat=(2, 1), transforms=[r], ax=ax)
     fig.savefig(datadir / 'test_bz.png')
     img1 = datadir / 'baseline' / 'rotated_bz.png'
     img2 = datadir / 'test_bz.png'
